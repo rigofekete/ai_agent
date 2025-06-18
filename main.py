76e7eacc7a25262c1b544c9a942ff68bd6301e26
rigofekete <fabricecosteira@gmail.com>
2025-06-18 14:45:50 +0200

    added instructions arg to the genai client call in main.py. Added prompt.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
 import sys
+from prompt import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-
+# we purposefully named this function as generate_content like the genai.Client method, because we are wrapping it with out own implementation and additional arguments 
 def generate_content(messages, client, verbose):
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                              contents=messages)
+    response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            # config will hold additional instructions to the model, as we are doing below
+            config=types.GenerateContentConfig(system_instruction=system_prompt)
+    )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
     if verbose:
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
-
 
     print("Response:")
     print(response.text)
@@ -32,7 +36,7 @@ def main():
 
     if not args:
         print("\n//// AI Code Assistant ////")
-        print('\nhow to use this AI agent? type: python main.py "type prompt here"')
+        print('\nhow to use this AI agent? type: python main.py "type prompt here" [--verbose]')
         sys.exit(1)
 
     user_prompt = " ".join(args)
